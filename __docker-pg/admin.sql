175894ac888f5a6a838732a8937d92039df1f7e6
Mayder1014 <facuelgueta@gmail.com>
2026-06-20 19:09:28 -0300

    Avances TP4

---
+++ __docker-pg/admin.sql
@@ -0,0 +1,37 @@
+-- Conectar a la base de datos 'carta'
+\c admin;
+
+DROP TABLE IF EXISTS carta;
+CREATE TABLE carta (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    precio NUMERIC(10, 2) NOT NULL,
+    categoria VARCHAR(50) NOT NULL,
+    imagen VARCHAR(255)
+);
+
+DROP TABLE IF EXISTS usuarios;
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(150) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    session_id VARCHAR(255) UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO carta (nombre, precio, categoria, imagen) VALUES
+    ('Mozzarella', 11000, 'Pizza', '/front-web/recursos/imagenes/productos/Pizza-mozzarella.webp'),
+    ('Calabresa', 13000, 'Pizza', '/front-web/recursos/imagenes/productos/Pizza-calabresa.webp'),
+    ('Napolitana', 14000, 'Pizza', '/front-web/recursos/imagenes/productos/Pizza-napolitana.webp'),
+    ('4 Quesos', 15000, 'Pizza', '/front-web/recursos/imagenes/productos/pizza-4_quesos.webp'),
+    ('Mixta', 15000, 'Pizza', '/front-web/recursos/imagenes/productos/Pizza-mixta.webp'),
+    ('Carne', 1250, 'Empanada', '/front-web/recursos/imagenes/productos/Empanada-criolla.webp'),
+    ('Pollo', 1250, 'Empanada', '/front-web/recursos/imagenes/productos/Empanada-pollo.webp'),
+    ('Verdura', 1300, 'Empanada', '/front-web/recursos/imagenes/productos/Empanada-verdura.webp'),
+    ('Jamón y Queso', 1350, 'Empanada', '/front-web/recursos/imagenes/productos/Empanada-jyq.webp'),
+    ('Coca Cola 1.5lt', 2500, 'Bebida', '/front-web/recursos/imagenes/productos/CocaComun.webp'),
+    ('Coca Cola Zero 1.5lt', 2600, 'Bebida', '/front-web/recursos/imagenes/productos/CocaZero.webp'),
+    ('Sprite 1.5lt', 2000, 'Bebida', '/front-web/recursos/imagenes/productos/Sprite.webp'),
+    ('Fanta 1.5lt', 2000, 'Bebida', '/front-web/recursos/imagenes/productos/Fanta.webp'),
+    ('Lata Quilmes 473ml', 2300, 'Bebida', '/front-web/recursos/imagenes/productos/lata-quilmes.webp'),
+    ('Schneider 710ml', 2800, 'Bebida', '/front-web/recursos/imagenes/productos/Schneider.webp');
--- index.mjs
+++ index.mjs
@@ -1,16 +1,22 @@
+// Modulos --------------------------------------------------------------------------------------------------
 import express from 'express'
 import path from 'node:path'
 import './iniciar.env.mjs'
 import rutasModuloProductos from './modulos/productos/rutas.productos.mjs'
+//
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
+import pool from './bbdd/conexion-bd.mjs'
 
-//Variables de entorno 
-//Como acceder?
-//console.log(process.env.puerto)
 
-//console.log(process.env.BD_PASS)
 const puerto = process.env.puerto || 3000
 
 const app = express()
+
+
+
+app.use(cookieParser(process.env.COOKIE_FIRMA))
 
 // Agregar estos middlewares ANTES de las rutas
 app.use(express.json())
@@ -19,11 +25,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(rutasModuloProductos)
 
 
-// Front Web
-app.use(express.static(path.resolve('./front-web')))
+// ..............................................
+function chequearAcceso(req, res, next){
+    const token = req.signedCookies['token']
 
-// Vinculamos el front
-app.use('/admin', express.static(path.resolve('./front-crud')))
+    jwt.verify(token, process.env.JWT_FIRMA, function(error, decoded){
+        if(error){
+            res.redirect('/login')
+        }
+        next()
+    })
+}
+// ..............................................
+
+
+// Servir el front Web (publico)
+app.use(express.static(path.resolve('./fronts/front-web')))
+
+// Servir el front crud (admin)
+app.use('/admin', chequearAcceso, express.static(path.resolve('./fronts/front-crud')))
+
+// Servir el front login (admin)
+app.use('/login', express.static(path.resolve('./fronts/front-login')))
 
 // Configuramos carpeta para servir achivos
 app.use('/archivos', express.static(path.resolve('./archivos')))
@@ -32,6 +55,107 @@ app.use('/archivos', express.static(path.resolve('./archivos')))
 app.use('/api', (req, res) => {
   res.status(404).json({mensaje: 'Endpoint de API no encontrado'})
 })
+
+
+
+
+// Configuración de rutas para autenticación y registro ---------------------------------------------------------------
+
+app.post('/autenticar', async (req, res)=>{
+    // 1- Obtener datos del formulario
+    const {usuario, pass} = req.body
+
+    // 2- Controlar datos incompletos
+    if(!usuario || !pass){
+        return res.status(404).json({
+            mensaje: "Datos incompletos."
+        })
+    }
+
+    // 3- Verificar si el usuario existe en la BBDD
+    const resultado = await pool.query(`
+        SELECT 
+            *
+        FROM 
+            usuarios
+        WHERE
+            username = $1
+        `, [usuario])
+
+    console.log(resultado.rows)
+
+    if (resultado.rowCount === 0){
+        return res.status(404).json({
+            mensaje: "Usuario no encontrado."
+        })
+    }
+
+    // 4- Verificar que la contraseña ingresada coincida con el hash guardado en la BBDD
+    const hash = resultado.rows[0].password_hash // --> Obtenemos el hash del usuario encontrado
+
+    const validacion = await bcrypt.compare(pass, hash); // --> Devuelve true o false
+
+    // 5- Si el booleano es false, mostrar mensaje de error, sino, generar cookie y redigir a admin
+    if (!validacion){
+        return res.status(404).json({
+            mensaje: "Contraseña incorrecta."
+        })
+    }
+    else{
+        // "Payload":
+        const datosUsuario = {
+            id: resultado.rows[0].id,
+            usuario: resultado.rows[0].username,
+            contraseña: resultado.rows[0].password_hash
+        };
+        
+        // La funcion jwt.sign() espera recibir un objeto "payload", por esa razón, anteriormente se crea uno conteniendo los datos del usuario.
+        const token = jwt.sign(datosUsuario, process.env.JWT_FIRMA, {expiresIn: '1h'})
+        
+        res.cookie('token', token, {
+            secure: true,
+            httpOnly: true,
+            sameSite: 'lax',
+            maxAge: 300000 // <-- (5 minutos)
+        })
+
+        return res.status(200).redirect('/admin')
+    } 
+})
+
+
+app.post('/registrar', async (req, res) => {
+    // 1- Obtener datos del formulario
+    const { usuario, pass } = req.body;
+
+    // 2- Controlar datos incompletos
+    if (!usuario || !pass) {
+        return res.sendStatus(400);
+    }
+
+    // 3- Hashear la contraseña ingresada por el usuario, y guardarlo junto al nombre en la base de datos
+    try {
+        const salt = bcrypt.genSaltSync(10);
+
+        const hashingPass = bcrypt.hashSync(pass, salt);
+
+        const resultado = await pool.query(
+            'INSERT INTO usuarios (username, password_hash) VALUES ($1, $2)',
+            [usuario, hashingPass]
+        );
+
+        if (resultado.rowCount > 0) {
+            res.redirect('/login'); // Redirigimos al usuario a la página de login
+        } else {
+            res.sendStatus(500);
+        }
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+
 
 app.listen(puerto, () => {
   console.log(`Servidor escuchando en http://localhost:${puerto}`)
